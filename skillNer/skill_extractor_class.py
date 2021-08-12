8722fe980cb6001f5b24e35e9322f4c512449d69
AnasAito <khdimaty@gmail.com>
2021-08-13 00:19:41 +0100

    add displayer

--- skillNer/skill_extractor_class.py
+++ skillNer/skill_extractor_class.py
@@ -1,7 +1,7 @@
 # native packs
 #
 # installed packs
-#
+from spacy import displacy
 # my packs
 from skillNer.text_class import Text
 from skillNer.matcher_class import Matchers, SkillsGetter
@@ -38,7 +38,7 @@ class SkillExtractor:
         self.utils = Utils(self.nlp, self.skills_db)
         return
 
-    def __call__(
+    def annotate(
         self,
         text: str
     ):
@@ -58,9 +58,49 @@ class SkillExtractor:
         n_gram_pro = self.utils.process_n_gram(skills_ngram, text_obj)
         uni_gram_pro = self.utils.process_unigram(skills_uni, text_obj)
 
-        return {
-            'full_match': skills_full,
-            'ngram_full_match': skills_sub_full,
-            'ngram_scored': n_gram_pro,
-            'unigram_scored': uni_gram_pro
-        }
+        return {'text': text_obj.transformed_text,
+                'results': {
+                    'full_match': skills_full,
+                    'ngram_full_match': skills_sub_full,
+                    'ngram_scored': n_gram_pro,
+                    'unigram_scored': uni_gram_pro
+                }}
+
+    def display(self, results):
+
+        text = results['text']
+        skill_extractor_results = results['results']
+        ents = []
+        matches = []
+        colors_id = []
+        colors = {}
+        tokens = text.split(' ')
+        skill_to_color = {'Hard Skill': '#818CF8', 'Soft Skill': '#F472B6'}
+
+        for match_type in skill_extractor_results.keys():
+            for match in skill_extractor_results[match_type]:
+                matches .append(match)
+
+        for match in matches:
+            entity = {}
+            start = sum(
+                [len(token)+1 for token in tokens[:match['doc_node_id'][0]]])
+
+            entity['start'] = start
+
+            end = start+sum(
+                [len(token)+1 for token in tokens[match['doc_node_id'][0]:1+match['doc_node_id'][-1]]])
+
+            entity['end'] = end
+            entity['label'] = self.skills_db[match['skill_id']]['skill_name']
+            colors[entity['label']
+                   ] = skill_to_color[self.skills_db[match['skill_id']]['skill_type']]
+            colors_id.append(entity['label'])
+            ents.append(entity)
+        ents.sort(key=lambda x: x['start'], reverse=False)
+        ex = [{"text": text,
+               "ents": ents,
+               "title": None}]
+
+        options = {"ents": colors_id, "colors": colors}
+        html = displacy.render(ex, style="ent", manual=True, options=options)
