46a2c82dbb172bfe90be3fcb3877d2e23ccf24da
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2021-08-15 13:18:04 +0100

    setup file added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+dependencies = [
+    "numpy",
+    "pandas",
+    "nltk",
+    "spacy",
+    "en_core_web_lg",
+]
+
+setuptools.setup(
+    name="skillNer",
+    version="0.0.1",
+    author="Anas Ait AOMAR / Badr MOUFAD",
+    author_email="pedroslashs@gmail.com",
+    description="A first deployed version of skillNer",
+    long_description=long_description,
+    url="https://github.com/AnasAito/SkillNER",
+    packages=setuptools.find_packages(),
+    install_requires=dependencies,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License ::  :: No license",
+        "Operating System :: OS Independent",
+    ],
+)
