08fc3c6507c368539a513b0f9bea4f69527094bd
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2021-10-25 19:57:42 +0100

    final setups to publish

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
 dependencies = [
     "numpy",
@@ -17,10 +15,9 @@ setuptools.setup(
     author="Anas Ait AOMAR & Badr MOUFAD",
     author_email="Badr.MOUFAD@emines.um6p.ma",
     description="An NLP module to automatically Extract skills and certifications from unstructured job postings, texts, and applicant's resumes",
-    long_description=long_description,
     url="https://github.com/AnasAito/SkillNER",
     keywords=['python', 'NLP', "NER", "skills-extraction", "job-description"],
-    # download_url = 'https://github.com/Badr-MOUFAD/test_package_badr_moufad/archive/refs/tags/v0.2.tar.gz',
+    download_url = 'https://github.com/AnasAito/SkillNER/archive/refs/tags/v1.0.0.tar.gz',
     packages=setuptools.find_packages(),
     install_requires=dependencies,
     classifiers=[
