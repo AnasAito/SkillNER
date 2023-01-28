a9316868d3ca75111b91a5e7901c408d4b38d2ad
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2023-01-28 20:28:47 +0100

    more and data structures and tests

--- skillner/core/data_structures.py
+++ skillner/core/data_structures.py
@@ -1,18 +1,11 @@
-from typing import List
-
-
-class Document:
-    """"""
-
-    def __init__(self) -> None:
-        pass
+from typing import List, Type
 
 
 class Word:
     """"""
 
-    def __init__(self, word) -> None:
-        self.word = word
+    def __init__(self, word: str) -> None:
+        self.word: str = word
 
     def __str__(self) -> str:
         return self.word
@@ -24,11 +17,27 @@ class Sentence:
     def __init__(self) -> None:
         self.li_words: List[Word]
 
-    def __getitem__(self, i):
+    def __getitem__(self, i) -> Type[Word]:
         return self.li_words[i]
 
     def __str__(self) -> str:
         return " ".join(str(word) for word in self)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.li_words)
+
+
+class Document:
+    """"""
+
+    def __init__(self) -> None:
+        self.li_sentences: List[Sentence]
+
+    def __getitem__(self, i) -> Type[Sentence]:
+        return self.li_sentences[i]
+
+    def __str__(self) -> str:
+        return ".\n".join(str(sentence) for sentence in self)
+
+    def __len__(self) -> int:
+        return len(self.li_sentences)
--- skillner/tests/test_data_strucutre.py
+++ skillner/tests/test_data_strucutre.py
@@ -1,15 +1,25 @@
-from skillner.core.data_structures import Sentence, Word
+from skillner.core.data_structures import Sentence, Word, Document
 
 
 def test_to_str():
-    sentence = Sentence()
+    greeting = "Hello and welcome to skillner".split()
+    purpose = "you can extract skills".split()
+    text = ".\n".join(
+        s for s in (" ".join(splitted_s) for splitted_s in (greeting, purpose))
+    )
 
-    greeting_sentence = "Hello and welcome to skillner".split()
-    sentence.li_words = [Word(w) for w in greeting_sentence]
+    doc = Document()
+    doc.li_sentences
 
-    assert len(sentence) == len(greeting_sentence)
-    assert str(sentence[2]) == greeting_sentence[2]
-    assert str(sentence) == " ".join(greeting_sentence)
+    for s in (greeting, purpose):
+        sentence = Sentence()
+        sentence.li_words = [Word(w) for w in s]
+        doc.li_sentences.append(sentence)
+
+    assert len(doc) == 2
+    assert len(doc[1]) == len(purpose)
+    assert str(doc[0]) == " ".join(greeting)
+    assert str(doc) == text
 
 
 if __name__ == "__main__":
